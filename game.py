7a1f0e15aade62adadad3ef36857931fdbae08cc
frankiephs <fb23419@student.phs.school.nz>
2023-08-17 18:33:55 +1200

    changes

--- game.py
+++ game.py
@@ -7,4 +7,20 @@ import pyrebase
 import settings
 
 
+# important notes. If calling a class attribute, make sure to not add parenthesis if not a method and also make sure you allocated the class into a vatiable.
 
+
+
+class game:
+    def __init__(self):
+        self.state = "intro"
+    def game_manager(self):
+        if self.state == "intro":
+        
+        if self.state   
+
+
+
+running = True
+while running:
+    # game loop
